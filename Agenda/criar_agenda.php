101de55faeb2e74867b59629a3d0aa4a048c2c03
Anaclarapereira <167783753+Anaclarapereira@users.noreply.github.com>
2024-11-01 17:37:26 -0300

    Add files via upload

--- Agenda/criar_agenda.php
+++ Agenda/criar_agenda.php
@@ -1,49 +1,61 @@
-<?php   
- include "connection.php";
- echo "<pre>";
- print_r($_POST);
+<?php
+include "../conexao.php";
 
- $inicio = strtotime($_POST['horario']);
- $final = strtotime($_POST['horario  ']);
- $mins = ($inicio - $final) / 60;
 
- if($mins < 0)
-  $mins = $mins*(-1);
+$inicio = strtotime($_POST['horario_inicio']);
+$final = strtotime($_POST['horario_saida_intervalo']);
+$dia_da_semana = $_POST['dia'];
+$id_usuario = $_POST['id_usuario'];
+$mins = ($inicio - $final) / 60;
+if($mins < 0)
+    $mins = $mins*(-1);
 //echo $mins;
 
 $qnt_de_agendamento = $mins/30;
 
-//echo "<br> qnt: $qnt-agendamento";
+//echo "<br> qnt: $qnt_de_agendamento";
 $acumulado = $inicio;
 
-echo "<h1>Geração de agendamentos do primeiro horário!</h1>";   
+echo "<h1>Geração de agendamentos do primeiro horário!</h1>";
 for($i = 0; $i < $qnt_de_agendamento; $i++){
 
-     echo "/n Criado agendamento no harario para: " . date('H:i', $acumulado) . "  do usuario";
-
+    echo "\n Criado agendamento no horario para: " . date('H:i', $acumulado);
+    $horario = date('H:i', $acumulado);
+    incluirAgenda($dia_da_semana, $id_usuario, $horario, $conn);
+    $acumulado = $acumulado + 1800;
 }
- 
+
+function incluirAgenda($dia_da_semana, $id_usuario, $horario, $conn){
+    $sql_agenda = "INSERT INTO agendas (dia_da_semana , id_usuario, horario)
+    VALUES ($dia_da_semana, $id_usuario, '$horario')";
+
+    if ($conn->query($sql_agenda) === TRUE) {
+    echo "New record created successfully";
+    } else {
+    echo "Error: " . $sql_agenda . "<br>" . $conn->error;
+    }
+}
 
 
- $barbeiro = $_POST['barbeiro'];
- $dia_da_semana = $_POST['dia'];
- $horario_de_inicio = $_POST['horario_inicio'];
- $horario_de_saida_intervalo = $_POST['horario_saida_intervalo'];
 
+$inicio = strtotime($_POST["horario_volta_intervalo"]);
+$final = strtotime($_POST["horario_fim"]);
+$mins = ($inicio - $final) / 60;
+if($mins < 0)
+    $mins = $mins*(-1);
+//echo $mins;
 
+$qnt_de_agendamento = $mins/30;
 
+//echo "<br> qnt: $qnt_de_agendamento";
+$acumulado = $inicio;
 
+echo "<h1>Geração de agendamentos do primeiro horário!</h1>";
+for($i = 0; $i < $qnt_de_agendamento; $i++){
 
+    echo "\n Criado agendamento no horario para: " . date('H:i', $acumulado);
+    $horario = date('H:i', $acumulado);
+    incluirAgenda($dia_da_semana, $id_usuario, $horario, $conn);
+    $acumulado = $acumulado + 1800;
+}
 
- $horario_de_volta_intervalo = $_POST['horario_volta_intervalo'];
- $horario_fim = $_POST['horario_fim'];
-
-echo "<br>barbeiro $barbeiro";
-echo "<br>dia_da_semana $dia";
-echo "<br>horario_de_inicio $horario_inicio";
-echo "<br>horario_de_saida_intervalo $horario_saida_intervalo";
-echo "<br>horario_de_volta_intervalo $horario_volta_intervalo";
-echo "<br>horario_fim $horario_fim";
-
-
- ?>
