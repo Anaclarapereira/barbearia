35a40eb8ec801b4291d1a987cb7fd027678644ef
Anaclarapereira <167783753+Anaclarapereira@users.noreply.github.com>
2024-11-27 18:02:11 -0300

    Add files via upload

--- Agenda/criar_agenda.php
+++ Agenda/criar_agenda.php
@@ -1,10 +1,10 @@
 <?php
-include "../conexao.php";
+include "../connection.php";
 
 
 $inicio = strtotime($_POST['horario_inicio']);
-$final = strtotime($_POST['horario_saida_intervalo']);
-$dia_da_semana = $_POST['dia'];
+$final = strtotime($_POST['horario_saida_intervelo']);
+$dia_da_semana = $_POST['dia_da_semana'];
 $id_usuario = $_POST['id_usuario'];
 $mins = ($inicio - $final) / 60;
 if($mins < 0)
@@ -37,10 +37,10 @@ function incluirAgenda($dia_da_semana, $id_usuario, $horario, $conn){
 }
 
 
-
-$inicio = strtotime($_POST["horario_volta_intervalo"]);
-$final = strtotime($_POST["horario_fim"]);
+$inicio = strtotime($_POST['horario_volta_intervela']);
+$final = strtotime($_POST['horario_fim']);
 $mins = ($inicio - $final) / 60;
+
 if($mins < 0)
     $mins = $mins*(-1);
 //echo $mins;
@@ -50,7 +50,7 @@ $qnt_de_agendamento = $mins/30;
 //echo "<br> qnt: $qnt_de_agendamento";
 $acumulado = $inicio;
 
-echo "<h1>Geração de agendamentos do primeiro horário!</h1>";
+echo "<h1>Geração de agendamentos do segundo horário!</h1>";
 for($i = 0; $i < $qnt_de_agendamento; $i++){
 
     echo "\n Criado agendamento no horario para: " . date('H:i', $acumulado);
@@ -58,4 +58,3 @@ for($i = 0; $i < $qnt_de_agendamento; $i++){
     incluirAgenda($dia_da_semana, $id_usuario, $horario, $conn);
     $acumulado = $acumulado + 1800;
 }
-
